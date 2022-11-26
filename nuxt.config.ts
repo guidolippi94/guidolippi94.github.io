02565f33db29ad6236e5e09009a91d8629cfb803
Guido Lippi <guidolippi94@gmail.com>
2022-11-26 15:52:18 +0100

    added gh pages config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: "/guidolippi94.github.io/",
+  },
   modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
